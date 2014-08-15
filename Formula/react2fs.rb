264432aede72f69fcc655dea75a64ab38da3e097
Jack Christensen <jack@jackchristensen.com>
2014-08-15 12:47:58 -0500

    Add react2fs formula

---
+++ Formula/react2fs.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class React2Fs < Formula
+  homepage "https://github.com/jackc/react2fs"
+  url "https://github.com/jackc/react2fs/archive/v0.1.1.tar.gz"
+  sha1 "3d862dca6c218ad564863216ca6dd1dfddefde4b"
+
+  depends_on "go" => :build
+
+  def install
+    # Homebrew is on Ruby 1.8.7 so cannot just pass env into system call
+    ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace"
+    system("go build -o react2fs")
+    bin.install("react2fs")
+  end
+end
