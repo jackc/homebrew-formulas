2b5732b556edb1241a2115cd9959036c3a55fdf9
Jack Christensen <jack@jackchristensen.com>
2015-05-15 20:57:20 -0500

    Update tern to 1.4.0

--- Formula/tern.rb
+++ Formula/tern.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Tern < Formula
   homepage "https://github.com/jackc/tern"
-  url "https://github.com/jackc/tern/archive/v1.3.3.tar.gz"
-  sha1 "68944d6d77c9b8638f05fabb98665eec6ec2ef70"
+  url "https://github.com/jackc/tern/archive/v1.4.0.tar.gz"
+  sha1 "b89218d7c0dadfda7f8200ac09630e92ab776f12"
 
   depends_on "go" => :build
 
